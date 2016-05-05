d9759df1d6bf7ce67d47dabc49877bb1d9ad8626
Meeka <diana.compagnone@gmail.com>
2016-05-04 23:02:54 -0400

    broke all the things!

---
+++ Assets/scripts/controls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controls : MonoBehaviour {
+
+
+    public delegate void horizontalMov(float direction);
+    public horizontalMov onHorizontalMov;
+
+    public delegate void verticalMov(float direction);
+    public verticalMov onVerticalMov;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizonal") != 0 && onHorizontalMov != null)
+        {
+            onHorizontalMov.Invoke(Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Vertical") != 0 && onVerticalMov != null)
+        {
+            onVerticalMov.Invoke(Input.GetAxis("Vertical"));
+        }
+	}
+}
---
+++ Assets/scripts/test.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public controls testControl;
+
+	// Use this for initialization
+	void Start () {
+        testControl.onHorizontalMov += horzMovEventReceived;
+        testControl.onVerticalMov += vertMovEventReceived;
+	}
+
+    private void vertMovEventReceived(float direction)
+    {
+        Debug.logger.Log("We moved vertically by this much: " + direction.ToString());
+    }
+
+    private void horzMovEventReceived(float direction)
+    {
+        Debug.logger.Log("We moved horizontally by this much: " + direction.ToString());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
