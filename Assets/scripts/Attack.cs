49076a7bd43de8d6ee10e8a93be654b754989fe2
Graham Knopp <graham@normli.ca>
2016-05-06 13:47:07 -0400

    graham's

---
+++ Assets/scripts/Attack.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+	public float damage;
+	public Animation charAnimation;
+
+	public Attack ()
+	{
+	}
+}
+
+
---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testCharacter : MonoBehaviour {
+
+	public Rigidbody body;
+
+	public float HP;
+	//controls controls
+	Attack currentAttack;
+
+	Animation idle;
+	Animation dead;
+
+	public Attack Normal1;
+	public Attack Normal2;
+	public Attack Special1;
+	public Attack Special2;
+
+	// Use this for initialization
+	void Start () {
+		/* controls.move += Move; */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Random.value);
+		if (Random.value < 0.01)
+			body.AddForce (new Vector3 (Random.value - 0.5f, Random.value -0.5f, Random.value-0.5f) * 500);
+	}
+
+	void DoAttack(Attack attack)
+	{
+		currentAttack = attack;
+
+	}
+
+
+}
