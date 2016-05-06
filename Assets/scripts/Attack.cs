dc2dfa7711e31b7445ddb440af104938708342c8
Graham Knopp <graham@normli.ca>
2016-05-06 17:33:02 -0400

    character and animation started

--- Assets/scripts/Attack.cs
+++ Assets/scripts/Attack.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
 	public float damage;
-	public AnimationClip charAnimation;
+	//public Anima charAnima
+	public Collider sourceChar;
 
 	public Attack ()
 	{
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other != sourceChar)
+			Debug.Log ("Hit! " + other.name);
 	}
 }
 
