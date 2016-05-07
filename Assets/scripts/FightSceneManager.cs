19ea067e44d28fba390b7120d3ae9d49931e9f86
Meeka <diana.compagnone@gmail.com>
2016-05-07 14:50:20 -0400

    Flipped player2 HP Bar, added ability to change HP

---
+++ Assets/scripts/FightSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightSceneManager : MonoBehaviour {
+
+	public RectTransform player1HP;
+	public RectTransform player2HP;
+
+	public float player1Percent = 1;
+	public float player2Percent = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		player1HP.pivot.Set ( 1/player1Percent, 0.5f);
+		player2HP.pivot.Set ( 1/player2Percent, 0.5f);
+	}
+}
